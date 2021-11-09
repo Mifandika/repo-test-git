54265be568a8e6d477885fdd4644c43d14108017
Mifandika <maulana.ifandika@gmail.com>
2021-11-09 20:06:39 +0700

    menambahkan folder dan file Main.cpp

---
+++ C++/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  int val;
+  cout << "enter number: ";
+  cin >> val;
+  
+  cout << "result: " << val << endl;
+  
+  cin.get();
+  return 0;
+}
